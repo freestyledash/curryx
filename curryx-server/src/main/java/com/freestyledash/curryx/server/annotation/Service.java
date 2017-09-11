e8da093468eaba35d48b245de9cf17fcc55c5c9e
freeStyleDash <freestyledash7@gmail.com>
2017-09-11 16:15:09 +0800

    修改注释

--- curryx-server/src/main/java/com/freestyledash/curryx/server/annotation/Service.java
+++ curryx-server/src/main/java/com/freestyledash/curryx/server/annotation/Service.java
@@ -9,9 +9,6 @@ import java.lang.annotation.Target;
 
 /**
  * 服务注解，被该注解修饰的类即服务端所提供的服务
- *
- * @author 郭永辉
- * @since 1.0 2017/4/4.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
