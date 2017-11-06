77d2621c2f41d16740b14343241e3b423b882a0b
freeStyleDash <freestyledash7@gmail.com>
2017-11-06 20:14:01 +0800

    Revert "完善log"
    This reverts commit 3b859f3077511266cd69bfe417fd24ba6a822460.

--- curryx-server/src/main/java/com/freestyledash/curryx/server/bootstrap/RPCServerBootstrap.java
+++ curryx-server/src/main/java/com/freestyledash/curryx/server/bootstrap/RPCServerBootstrap.java
@@ -12,7 +12,7 @@ public class RPCServerBootstrap {
     /**
      * 启动server
      *
-     * @param context spring实例
+     * @param context
      */
     public static void launch(ApplicationContext context) {
         context.getBean(RPCServer.class).start();
@@ -21,7 +21,7 @@ public class RPCServerBootstrap {
     /**
      * 新建线程启动server
      *
-     * @param context spring实例
+     * @param context
      */
     public static void launchInNewThread(ApplicationContext context) {
         new Thread(() -> {
